2077c96ccf194f63fc2b6d3b7db24dc0a3a838c8
terivan <terivan2006@yandex.ru>
2025-02-12 22:16:19 -0800

    Added a readiness handler

--- main.go
+++ main.go
@@ -1,29 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-func main(){
+func ReadinessHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func main() {
 	mux := http.NewServeMux()
 
 	server := http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
 
-
-	mux.Handle("/", http.FileServer(http.FileSystem(http.Dir("."))))
-
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", ReadinessHandlerFunc)
 
 	err := server.ListenAndServe()
 
-	if err != nil{
-		fmt.Println("Couldn't run server!")
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
 		return
 	}
-
 
 	return
 }
